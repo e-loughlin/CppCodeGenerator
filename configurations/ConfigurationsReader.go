e0a7a3bf4731e9dea69af234aa0beb347aa8be95
Evan Loughlin <emloughl@gmail.com>
2019-11-01 16:18:20 -0600

    Added error handling and user info for failed read of config.json

--- configurations/ConfigurationsReader.go
+++ configurations/ConfigurationsReader.go
@@ -2,6 +2,7 @@ package configurations
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/emloughl/CppCodeGenerator/util"
 )
@@ -10,7 +11,11 @@ func readConfigurations() Configurations {
 	var config Configurations
 	jsonData := []byte(util.ReadContents(util.ConfigurationsPath))
 	err := json.Unmarshal(jsonData, &config)
-	util.Check(err)
+	
+	if(err != nil) {
+		fmt.Println("Unable to successfully read config.json. Check that it is valid.")
+		util.Check(err)
+	}
 
 	return config
 }
