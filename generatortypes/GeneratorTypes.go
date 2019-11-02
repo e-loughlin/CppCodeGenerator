fdd32d9037277378e41400ffc1fb0460037bc6c6
Evan Loughlin <emloughl@gmail.com>
2019-11-01 18:42:21 -0600

    Added Enum for GeneratorTypes (the type of file to be created)

---
+++ generatortypes/GeneratorTypes.go
@@ -0,0 +1,34 @@
+package generatortypes
+
+import (
+	"strings"
+)
+
+type GeneratorType int 
+
+const (
+	Interface GeneratorType = iota
+	Class
+	Test
+	Mock
+	SpyMock
+	Unknown
+)
+
+func GetGeneratorType(inputString string) GeneratorType {
+	inputString = strings.ToLower(inputString)
+	switch(inputString) {
+	case "interface":
+		return Interface
+	case "class":
+		return Class
+	case "test":
+		return Test
+	case "mock":
+		return Mock
+	case "spymock":
+		return SpyMock
+	default:
+		return Unknown
+	}
+}
