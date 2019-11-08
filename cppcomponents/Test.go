62885585c06081f71dd29e8bac66f5040c496d89
Evan Loughlin <evan.loughlin@circlecvi.com>
2019-11-08 14:57:40 -0700

    Introduced Test class

---
+++ cppcomponents/Test.go
@@ -0,0 +1,30 @@
+package cppcomponents
+
+import (
+	"github.com/emloughl/CppCodeGenerator/util/configurations"
+)
+
+// Test ... Implements File
+type Test struct {
+	ClassImplementation ClassImplementation
+	TestName          		string
+	ConcreteName			string
+	ConcreteFileName		string
+}
+
+func NewTestByConcreteName(concreteName string) *Test {
+	t := Test{}
+	t.TestName = configurations.Config.Prefixes.Test + concreteName + configurations.Config.Suffixes.Test
+	t.ConcreteName = concreteName
+	t.ConcreteFileName = concreteName + configurations.Config.FileExtensions.CppHeader
+	return &t
+}
+
+// Fields ... The fields within templates to be replaced.
+func (t Test) Fields() map[string]string {
+	fields := make(map[string]string)
+	fields["{{Test.ConcreteFileName}}"] = t.ConcreteFileName
+	fields["{{Test.ConcreteName}}"] = t.ConcreteName
+	fields["{{Test.Name"] = t.TestName
+	return fields
+}
