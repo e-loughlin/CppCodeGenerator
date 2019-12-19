589d38059555f5badb4bb46bb97948bd0420730a
Evan Loughlin <evan.loughlin@circlecvi.com>
2019-12-19 15:39:05 -0700

    Updated licensing of gmock_gen_wrapper.py to reflect that file was created by copying gmock_gen.py

--- resources/gmock_generator/gmock_gen_wrapper.py
+++ resources/gmock_generator/gmock_gen_wrapper.py
@@ -1,14 +1,28 @@
-
+#!/usr/bin/env python
+#
+# Copyright 2008 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Evan Loughlin, via copying gmock_gen.py and wrapping additionally functionality around it.
+# All rights retained by original author of gmock_gen.py.
 
 """CppCodeGenerator wrapper of gmock_gen.py to modify the interface prior to having gmock_gen.py use it"""
-
-__author__ = 'nnorwitz@google.com (Neal Norwitz)'
 
 import os
 import sys
 import gmock_gen
 
-# TODO: Figure out why this isn't working. Try on I_SudokuPuzzle.h
 
 def removeSignalsFromInterface(interfaceContents):
     updatedContents = []
